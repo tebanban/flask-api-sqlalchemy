403f114608924e78aeef9f9523999d2d049b38cd
tebanban <estebancalf@gmail.com>
2021-08-22 21:10:55 -0600

    app.py completed

--- src/app.py
+++ src/app.py
@@ -1,20 +1,20 @@
-from enum import unique
-from flask import Flask, request
+
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 
-app=Flask (__name__)
-
-app.config['SQL_ALCHEMY_DATABASE_URI']='mysql+pymysql://root@localhost/flaskmysql'
-app.config['SQL_ALCHEMY_TRACK_MOFIFICATIONS']=False
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root@localhost/flaskmysql'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 ma = Marshmallow(app)
 
+
 class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(80), unique=True)
-    description= db.Column(db.String(100))
+    title = db.Column(db.String(70), unique=True)
+    description = db.Column(db.String(100))
 
     def __init__(self, title, description):
         self.title = title
@@ -24,30 +24,62 @@ db.create_all()
 
 class TaskSchema(ma.Schema):
     class Meta:
-        fields = ('id' , 'title' , 'description')
+        fields = ('id', 'title', 'description')
+
 
 task_schema = TaskSchema()
-tasks_schema = TaskSchema (many=True)
+tasks_schema = TaskSchema(many=True)
 
-@app.route('/tasks' , methods=['POST'])
+@app.route('/tasks', methods=['Post'])
 def create_task():
+  title = request.json['title']
+  description = request.json['description']
 
-    title= request.json['title']
-    description= request.json['description']
+  new_task= Task(title, description)
 
-    new_task = Task(title,description)
-    db.session.add(new_task)
-    db.session.commit()
+  db.session.add(new_task)
+  db.session.commit()
 
-    return task_schema.jsonify(new_task)
+  return task_schema.jsonify(new_task)
 
-@app.route('/tasks' , methods=['GET'])
-def get_task():
+@app.route('/tasks', methods=['GET'])
+def get_tasks():
+  all_tasks = Task.query.all()
+  result = tasks_schema.dump(all_tasks)
+  return jsonify(result)
 
-    print(request.json)
-    return 'get from the server'
+@app.route('/tasks/<id>', methods=['GET'])
+def get_task(id):
+  task = Task.query.get(id)
+  return task_schema.jsonify(task)
 
-    
+@app.route('/tasks/<id>', methods=['PUT'])
+def update_task(id):
+  task = Task.query.get(id)
+
+  title = request.json['title']
+  description = request.json['description']
+
+  task.title = title
+  task.description = description
+
+  db.session.commit()
+
+  return task_schema.jsonify(task)
+
+@app.route('/tasks/<id>', methods=['DELETE'])
+def delete_task(id):
+  task = Task.query.get(id)
+  db.session.delete(task)
+  db.session.commit()
+  return task_schema.jsonify(task)
+
+
+@app.route('/', methods=['GET'])
+def index():
+    return jsonify({'message': 'Welcome to my API'})
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
